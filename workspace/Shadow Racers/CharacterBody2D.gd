4a4b4abb65151778cfbfc7229bf0998a98a75165
ToaBollua <bollua@proton.me>
2024-07-24 19:26:26 -0400

    Comments added

--- workspace/Shadow Racers/CharacterBody2D.gd
+++ workspace/Shadow Racers/CharacterBody2D.gd
@@ -1,10 +1,31 @@
 extends CharacterBody2D
 
+"""
+
+This script handles the player movement and it's animation.
+
+It assumes that the player node has an AnimatedSprite2D child node.
+It detects user input using the gdscript's input method
+(Customizable via Godot's GUI) to move the player and
+animate them.
+
+"""
+
 @onready var anim = $AnimatedSprite2D
 @onready var sprite = $AnimatedSprite2D
 
 var speed = 100
 
+
+"""
+
+This functon and it's
+processes are used to check the user's
+input and make the player's character
+react to them as it needs, according to
+already defined physics and speed variables.
+
+"""
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_left"):
 		Timer.start
